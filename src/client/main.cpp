91bb60f7b23daf4e72ccd886793a7464b7ed7dd5
James Ferrarelli <ferrarellijames@gmail.com>
2024-05-30 02:26:17 -0400

    Simplistic client audio playing
    Only works in debugger - kind of
    Massive clicks and pops in the left channel.
    
    Segfaults when running the normal debug build.
    
    Included song file I produced.

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -7,10 +7,50 @@
 
 #include <opus.h>
 #include <opusfile.h>
+#include <SDL2/SDL.h>
 
 #include <string>
 
 namespace javelin {
+
+float audio_buffer[2048];
+int audio_offset = 0;
+float volume = 0.4f;
+
+void audio_callback(void *, uint8_t *stream, int length) {
+	SDL_memset(stream, length, 0);
+
+	SDL_memcpy(stream, audio_buffer + audio_offset, length);
+	audio_offset += length;
+	audio_offset %= 2048;
+}
+
+void play_audio(const SDL_AudioSpec &spec) {
+	// TODO: maybe make a client argument or file chooser and check path validity
+	const char *path = "bets_are_off.opus"; // have to run from project root
+
+	int opus_error;
+	OggOpusFile *file = op_open_file(path, &opus_error);
+	if (opus_error) {
+		fmt::print(stderr, "ERROR: Could not open OggOpusFile\n");
+		return;
+	}
+
+	size_t samples_left = op_pcm_total(file, -1); // use a negative _li value to read the whole stream length
+	fmt::print("status: Sample rate {}\n", spec.freq);
+
+	while (samples_left > 0) {
+		uint32_t samples_read = op_read_float(file, audio_buffer, sizeof(audio_buffer) * sizeof(audio_buffer[0]), NULL);
+		samples_left -= samples_read;
+		fmt::print("status: {} samples read\n", samples_read);
+
+		uint32_t wait_time = float(samples_read) / (float(spec.freq) / 1000.f);
+		fmt::print("status: waiting {}ms\n", wait_time);
+		SDL_Delay(wait_time);
+	}
+
+	op_free(file);
+}
 
 int client_main() {
 	const uint32_t port = 34345;	
@@ -22,6 +62,28 @@ int client_main() {
 	address.sin_family = PF_INET;
 	address.sin_port = htons(port); // this can be 0, and will auto assign random port
 	address.sin_addr.s_addr = INADDR_ANY;
+
+	if (SDL_Init(SDL_INIT_AUDIO)) {
+		fmt::print(stderr, "ERROR: Could not init sdl\n{}\n", SDL_GetError());
+		return 1;
+	}
+
+// SDL doesnt want its user to fill the missing fields
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+	SDL_AudioSpec obtained_spec;
+	SDL_AudioSpec requested_spec = {
+		.freq = 48000,
+		.format = AUDIO_F32,
+		.channels = 2,
+		.samples = 2048,
+		.callback = audio_callback,
+		.userdata = nullptr,
+	};
+#pragma GCC diagnostic pop
+
+	SDL_AudioDeviceID audio_device = SDL_OpenAudioDevice(NULL, false, &requested_spec, &obtained_spec, SDL_AUDIO_ALLOW_ANY_CHANGE);
+	SDL_PauseAudioDevice(audio_device, false);
+	play_audio(obtained_spec);
 
 	if (connect(client_socket, (struct sockaddr*)&address, sizeof(address))) {
 		fmt::print(stderr, "ERROR: failled to connect to socket\n");
@@ -48,6 +110,8 @@ int client_main() {
 
 
 	close(client_socket);
+
+	SDL_Quit();
 
 	return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,6 @@
 #include <client/client.h>
 
 int main() {
-	if (SDL_Init(SDL_INIT_AUDIO)) {
-		fmt::print(stderr, "ERROR: Could not init sdl\n{}\n", SDL_GetError());
-		return 1;
-	}
-
 	//javelin::server_main(); // TODO: run in separate thread
 	javelin::client_main();
-
-	SDL_Quit();
 }
