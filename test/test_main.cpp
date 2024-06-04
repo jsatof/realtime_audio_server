322e312243439c411cfbbf4eddd00fcbf6fdfd8b
James Ferrarelli <ferrarellijames@gmail.com>
2024-06-04 12:45:16 -0400

    Unit tests for ring_buffer

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,8 +1,38 @@
 #include <gtest/gtest.h>
-#include <stub.h>
+#include <ring_buffer.h>
+#include <fmt/core.h>
 
-TEST(StubTestSuite, AlwaysPasses) {
-    EXPECT_EQ(alwaysTrue(), true);
+TEST(AudioServerTestSuite, RingBufferInitTest) {
+	auto ring_buffer = javelin::RingBuffer();
+	ASSERT_TRUE(ring_buffer.size == 12288);
+	ASSERT_TRUE(ring_buffer.head == 0);
+	ASSERT_TRUE(ring_buffer.tail == 0);
+}
+
+TEST(AudioServerTestSuite, RingBufferNormalInsertTest) {
+	auto ring_buffer = javelin::RingBuffer();
+
+	float dummy_data[12];
+	ring_buffer.write(dummy_data, sizeof(dummy_data));
+	ASSERT_TRUE(ring_buffer.head == 0);
+	ASSERT_TRUE(ring_buffer.tail == sizeof(dummy_data));
+
+	ring_buffer.write(dummy_data, sizeof(dummy_data));
+	ASSERT_TRUE(ring_buffer.head == sizeof(dummy_data));
+	ASSERT_TRUE(ring_buffer.tail == 2 * sizeof(dummy_data));
+}
+
+TEST(AudioServerTestSuite, RingBufferLoopbackTest) {
+	auto ring_buffer = javelin::RingBuffer();
+	ring_buffer.head = 12274;
+	ring_buffer.tail = 12280; // 8 before the end
+
+	float dummy_data[12];
+	ring_buffer.write(dummy_data, sizeof(dummy_data));
+	ASSERT_TRUE(ring_buffer.head = 12280);
+	ASSERT_TRUE(ring_buffer.tail == 4);
+
+
 }
 
 int main(int argc, char **argv) {
